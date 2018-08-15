20ccc3aae0d99c4bdedfc7132c23df2cc5ec3328
MineBitOfficial <42404505+MineBitOfficial@users.noreply.github.com>
2018-08-15 18:12:42 +0800

    fix a bug

--- MineBit/minebit.php
+++ MineBit/minebit.php
@@ -7,45 +7,39 @@ class minebit extends minebit_base{
 		parent::__construct($authentication);
 	}
 
-	//获取MineBit行情
+	//MineBit ticker
 	public function tickerApi($params = null) {
 		return $this -> post("/openapi/v1/market/quote", $params);
 	}
 	
-	//下单交易
+	//place an order
 	public function tradeApi($params = null) {
 		return $this -> post("/openapi/v1/trade/order", $params);
 	}
 
 
-	//撤销订单
+	//cancel an order
 	public function cancelOrderApi($params = null) {
 		return $this -> post("/openapi/v1/trade/cancel_order", $params);
 	}
 
-	//获取用户的订单信息
+	//get order info 
 	public function orderInfoApi($params = null) {
 		return $this -> post("/openapi/v1/trade/order_info", $params);
 	}
 
 
-	//获取市场深度
+	//get market depth
 	public function depthApi($params = null) {
 		return $this -> post("/openapi/v1/trade/depth", $params);
 	}
-	//提币
+	//withdrawl
 	public function withdrawApi($params = null) {
 		return $this -> post("/openapi/v1/wallet/withdraw", $params);
 	}
 	
-	//取消提币
+	//cancel withdrawl
 	public function cancelWithdrawApi($params = null) {
 		return $this -> post("/openapi/v1/wallet/cancel_withdraw", $params);
 	}
-
-	//获取美元人民币汇率
-	public function getUSD2CNYRateApi($params = null) {
-		return $this -> post("/openapi/v1/market/change_rate", $params);
-	}
-
 }
